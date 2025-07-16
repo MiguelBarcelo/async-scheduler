efbdc3bd319bf6fc651442c0a60b402529ff33b0
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-15 22:49:33 -0700

    feat: enable Scheduling in App

--- src/main/java/com/miguel_barcelo/async_scheduler/AsyncSchedulerApplication.java
+++ src/main/java/com/miguel_barcelo/async_scheduler/AsyncSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.miguel_barcelo.async_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AsyncSchedulerApplication {
 
 	public static void main(String[] args) {
