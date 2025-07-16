4d97b5f1e0686b46ad685f1ff6af16f637091d94
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-15 20:35:11 -0700

    feat: init project

---
+++ src/main/java/com/miguel_barcelo/async_scheduler/AsyncSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel_barcelo/async_scheduler/AsyncSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
